28e2485e01a40bddd73d83d8f08e1c74a1d37fb2
Aung Khaing Khant <aungkhaingkhant.dev@gmail.com>
2023-02-01 21:02:14 +0630

    Implements bestLanguage()
    This function returns the name of the best programming language as a string,

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ class NgaPaw{
         const random = Math.floor(Math.random() * this.phrases.length);
         return this.phrases[random]
     }
+    ///Tells the user what is the best programming language.
+    bestLanguage(){
+        return "Java";
+    }
 }
 
 module.exports = NgaPaw;
